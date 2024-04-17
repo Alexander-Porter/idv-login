569e89ad72d03922afee3ce19207a0855f12e7f6
liubairun <33106786+liubairun@users.noreply.github.com>
2024-04-17 12:49:08 +0800

    Update run.bat
    附带启动游戏命令

--- run.bat
+++ run.bat
@@ -1,4 +1,21 @@
- cd /d %~dp0
- echo "登录成功，且进入游戏后再关闭本程序"
- mitmweb -s netease.py --mode transparent --allow-hosts service.mkey.163.com --set block_global=false
- pause
+@echo off  
+title 第五人格账号密码登录
+color 0A
+setlocal enabledelayedexpansion  
+ 
+for /f "delims=" %%a in (path.txt) do (  
+    set "fullPath=%%a"
+REM 读取path.txt内容到变量fullpath
+)
+for %%F in ("%fullPath%") do (  
+    set "dirPath=%%~dpF"  
+    cd /d "!dirPath!" 
+REM 读取游戏目录
+	start dwrg.exe
+	echo 请在登入游戏后关闭本窗口
+) 
+
+endlocal
+:: 后台静默启动mitmweb  
+mitmweb -s netease.py --mode transparent --allow-hosts service.mkey.163.com --set block_global=false --no-web-open-browser -q
+pause
