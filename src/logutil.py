0b84b26ee4c96e58f0c4ecbbba8478a040af1343
AlexanderPorter <AlexanderPorter@126.com>
2024-05-05 20:59:46 +0800

    添加日志

---
+++ src/logutil.py
@@ -0,0 +1,38 @@
+# coding=UTF-8
+"""
+ Copyright (c) 2024 Alexander-Porter & fwilliamhe
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+ """
+
+import logging
+
+def setup_logger(module_name):
+    logger = logging.getLogger(module_name)
+
+    file_handler = logging.FileHandler('log.txt')
+    file_handler.setLevel(logging.DEBUG)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    logger.setLevel(logging.DEBUG)
+
+    return logger
