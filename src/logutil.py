32395dbec387be03bf392d175e71f0e5c330becd
Alexander-Porter <alexanderporter@126.com>
2024-05-05 08:14:34 -0700

    修复了一些人畜无害的小logging bug

--- src/logutil.py
+++ src/logutil.py
@@ -21,12 +21,11 @@ import logging
 def setup_logger(module_name):
     logger = logging.getLogger(module_name)
 
-    file_handler = logging.FileHandler('log.txt')
+    file_handler = logging.FileHandler('log.txt',encoding='utf-8')
     file_handler.setLevel(logging.DEBUG)
     file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
-
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
