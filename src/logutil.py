d0c0c17a78d05af7300965cfbcd7390ee4c83beb
Alexander-Porter <88940917+Alexander-Porter@users.noreply.github.com>
2024-05-06 19:04:45 +0800

    增加对Hosts的判断

--- src/logutil.py
+++ src/logutil.py
@@ -23,12 +23,12 @@ def setup_logger(module_name):
 
     file_handler = logging.FileHandler('log.txt',encoding='utf-8')
     file_handler.setLevel(logging.DEBUG)
-    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s - %(message)s')
     file_handler.setFormatter(file_formatter)
     logger.addHandler(file_handler)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s - %(message)s')
     console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
 
