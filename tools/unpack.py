7a47eca7918ed586b42a1670e7aed3f68e5deeb9
Alexander-Porter <alexanderporter@126.com>
2024-07-30 18:08:43 +0800

    fix auto pkg tool

--- tools/unpack.py
+++ tools/unpack.py
@@ -4,8 +4,10 @@ import json
 import os,subprocess,re
 def getNeteaseGameInfo(apkPath):
     app_channel = None
+    application=None
     package_name = None
     log_key=None
+    game_id=None
     #jadx apkPath --single-class com.netease.dwrg.Channel   --output-format json
     #use jadx to get package name
     os.makedirs('res', exist_ok=True)
@@ -26,13 +28,6 @@ def getNeteaseGameInfo(apkPath):
         root = ET.fromstring(data)
         package_name = root.attrib['package']
         application = root.find('application')
-        
-        # Define the 'android' namespace prefix
-        namespaces = {'android': 'http://schemas.android.com/apk/res/android'}
-        if app_channel=='xiaomi_app':
-            meta_data = application.find("./meta-data[@android:name='miGameAppId']", namespaces)
-            miGameAppId = meta_data.attrib['{http://schemas.android.com/apk/res/android}value']
-
         #use regular expression to get package name com.netease.???.xxxx -> com.netease.???
         package_name=re.search(r'(com\.netease.+?)\.\w+',package_name).group(1)
     subprocess.check_call([jadx_path, apkPath, '--single-class', f'{package_name}.Channel', '--output-format', 'json','--single-class-output', 'res/Channel.json'])
@@ -55,6 +50,7 @@ def getNeteaseGameInfo(apkPath):
                         game_id = match.group(1)
 
     if app_channel=='xiaomi_app':
+        namespaces = {'android': 'http://schemas.android.com/apk/res/android'}
         meta_data = application.find("./meta-data[@android:name='miGameAppId']", namespaces)
         miGameAppId = meta_data.attrib['{http://schemas.android.com/apk/res/android}value']
         channelData=miGameAppId
@@ -69,4 +65,5 @@ def getNeteaseGameInfo(apkPath):
     RES["game_id"]=game_id
     RES[app_channel]=channelData
     print(RES)
+    print(json.dumps(RES))
 getNeteaseGameInfo("app.apk")
